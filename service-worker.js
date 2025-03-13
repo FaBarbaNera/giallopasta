8df0ba7aa3d05d9437a6b2cc385d231c941d85ca
FaBarbaNera <fabiogiorgio84@yahoo.it>
2025-03-13 21:11:11 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,30 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('my-cache').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/styles.css',
-          '/script.js',  // Modifica con i tuoi file
-          '/icons/icon-192x192.png',
-          '/icons/icon-512x512.png'
-        ]);
-      })
-    );
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request).then(function(cachedResponse) {
-        return cachedResponse || fetch(event.request);
-      })
-    );
-  });
-  
+// Cache a specific list of resources
+self.addEventListener('install', (event) => {
+  console.log('Service Worker Installed');
+  event.waitUntil(
+    caches.open('giallopasta-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/menu.html',
+        '/recensioni.html',
+        '/contatti.html',
+        '/style.css',
+        '/app.js',
+        '/images/logo1.png',
+        '/images/screenshot1.jpg',
+        '/images/screenshot2.jpg'
+      ]);
+    })
+  );
+});
+
+// Intercetta le richieste di rete e prova a servire dalla cache
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      // Se c'è una risposta nella cache, restituiscila; altrimenti, fai una richiesta di rete
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
